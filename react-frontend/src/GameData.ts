39143d73a3dbd85f231936f92d316f5142382af8
anthony <anthony@win11>
2022-08-23 01:27:31 -0500

    Move interface delcaration to separate file to share

---
+++ react-frontend/src/GameData.ts
@@ -0,0 +1,9 @@
+export interface GameData {
+  url: string,
+  name: string,
+  description: string,
+  date_updated: string,
+  checked_name: string,
+  checked_description: string,
+  checked_date_updated: string
+}
