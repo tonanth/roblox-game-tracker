d58971ca4b5e7b4d9d62ce69c331362006ff9f4e
anthony <anthony@win11>
2022-08-22 23:17:52 -0500

    Move server code to be together with the react frontend

--- react-frontend/express-server.js
+++ react-frontend/express-server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const cors = require('cors')
 
 const app = express();
-app.use(express.json)
-app.use()
-const port = 4321;
+app.use(express.json())
+app.use(cors())
+const port = 3001;
 
 const pool = new Pool({
   user: 'postgres',
@@ -20,25 +20,41 @@ pool.on('error', (err, client) => {
   process.exit(-1)
 });
 
-(async () => {
-  const client = await pool.connect() 
-  try {
-    const res = await client.query('select * from roblox_game_tracker')
-    console.log(res.rows[0])
-  } finally {
-    client.release()
-  }
-})().catch(err => console.log(err.stack)) 
+// (async () => {
+//   const client = await pool.connect() 
+//   try {
+//     const res = await client.query('select * from roblox_game_tracker')
+//     console.log(res.rows[0])
+//   } finally {
+//     client.release()
+//   }
+// })().catch(err => console.log(err.stack)) 
 
 app.get('/status', (req, res) => {
   res.send('roblox-game-tracker');
 })
 
-app.put('/api/addgame', async (req, res) => {
+app.get('/api/getgamedata'), async (req, res) => {
+  
+}
 
+app.put('/api/addgameurl', async (req, res) => {
+  values = {url : req.body.url}
   const client = await pool.connect();
   const query = 'insert into roblox_game_tracker (url) values ($1) on duplicate key update url=url'
   try {
-    const res = await client.query(query)
+    const res = await client.query(query, values);
+  } catch(err) {
+    console.log(err.stack);
+  } finally {
+    client.release();
   }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log('listening')
 })
