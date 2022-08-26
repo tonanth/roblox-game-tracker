20f39e4f41f4188566de0ae7065adc547d69ec9e
anthony <anthony@win11>
2022-08-25 19:04:28 -0500

    Change url display to show name and not raw URL

--- react-frontend/src/GamePanel.tsx
+++ react-frontend/src/GamePanel.tsx
@@ -5,14 +5,17 @@ interface Props {
 }
 
 function GamePanel(props: Props) {
-  const gameData = props.gameData
+  const gameData = props.gameData;
   return (
     <div className="game-panel">
-      <h1>{gameData.url}</h1>
+      <h1 className='game-url'><a href={gameData.url}>{gameData.name}</a></h1>
+      <hr />
       <h2>Name</h2>
       <p>{gameData.name}</p>
+      <hr />
       <h2>Description</h2>
       <p>{gameData.description}</p>
+      <hr />
       <h2>Date Updated</h2>
       <p>{gameData.date_updated}</p>
     </div>
